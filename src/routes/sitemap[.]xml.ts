1eb745a712dcd7825308e5f16b636b0f34b927eb
MENOVO- From Table to Screen. <7menovo@gmail.com>
2026-09-05 22:35:52 +0300

    Update sitemap[.]xml.ts

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -1,8 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
 
-const BASE_URL = "https://menovo.lovable.app";
-
+const BASE_URL = "https://www.menovo.agency";
 export const Route = createFileRoute("/sitemap.xml")({
   server: {
     handlers: {
