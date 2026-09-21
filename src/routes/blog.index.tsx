e5c8cc66d9c970415233d65989ab384156c02a8e
MENOVO- From Table to Screen. <7menovo@gmail.com>
2026-09-21 17:16:35 +0300

    Polish frontend blog glass cards

--- src/routes/blog.index.tsx
+++ src/routes/blog.index.tsx
@@ -67,7 +67,7 @@ function BlogPage() {
                 <Link
                   to="/blog/$slug"
                   params={{ slug: p.slug }}
-                  className="glass-card group block h-full overflow-hidden rounded-[2rem] border border-white/10 p-5 transition-all duration-500 hover:-translate-y-2 hover:border-white/20 hover:shadow-2xl hover:shadow-black/20"
+                  className="glass-card group block h-full overflow-hidden rounded-[2rem] border border-white/10 bg-white/[0.04] p-5 shadow-xl shadow-black/10 backdrop-blur-xl transition-all duration-500 hover:-translate-y-2 hover:border-white/20 hover:bg-white/[0.065] hover:shadow-2xl"
                 >
                 {p.featured_image_url && (
                   <img
